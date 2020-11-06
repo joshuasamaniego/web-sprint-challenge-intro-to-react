95ad5d9a45674ea8bac573d2176df8276572311a
Joshua Samaniego- Evans <joshuasamaniego-evans@Joshuas-MacBook-Pro.local>
2020-11-06 09:43:32 -0700

    Failed

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import App from './App.js';
+import axios from 'axios';
+import './index.css';
+
+export default function Character(props) {
+    const [character, setCharacter] = useState({});
+
+    useEffect(() => {
+
+    });
+}
