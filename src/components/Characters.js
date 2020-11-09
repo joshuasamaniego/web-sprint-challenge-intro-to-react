f0fa3ef903a3090c30071d5a5389f69f4773b8d8
Joshua Samaniego- Evans <joshuasamaniego-evans@Joshuas-MacBook-Pro.local>
2020-11-08 19:21:27 -0700

    has some cards showing with some basic use of styled components

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,23 +1,38 @@
 // Write your Character component here
-//import axios from 'axios';
 import React from 'react';
+import styled from "styled-components";
 
 export default function Characters(props) {
-
     const { characters } = props;
-
-    console.log(characters);
-
     return (
-       <div>
-            { characters && characters.map((character, index) => (
-               <img src={character.image} alt="" key={index}></img>
-            ))}
-            { characters && characters.map((character, index) => (
-                <li key={index}>
-                {character.name}
-                </li>
-            ))}
-       </div>
+       <StyledContainer>
+            { characters.map((character, id) => (
+                <StyledCharactersDiv key={id}>
+                    <img src={character.image} alt=""></img>
+                    <li>{character.name}</li>
+                    <li>{character.species}</li>
+                    <li>{character.gender}</li>
+                </StyledCharactersDiv>
+            )) }
+       </StyledContainer>
     );
 }
+
+const StyledContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
+
+const StyledCharactersDiv = styled.div`
+    width: 20%;
+    height: 50vh;
+    padding: 2%;
+    margin: 3%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+    background-color: #333;
+    color: white;
+`
